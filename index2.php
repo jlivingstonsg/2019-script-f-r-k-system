379b027d3d21dcb2508206e5d74fce3da4b33605
jlivingstonsg <magnus.ivarsson@gmail.com>
2019-09-08 14:38:39 +0200

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+<iframe height="800px" width="100%" src="http://192.168.1.165:8088/login.html" name="iframe_a"  style="border:none;"></iframe>
+
+<script>
+         setTimeout(function(){
+            window.location.href = 'https://www.gamereality.se';
+         }, 20000);
+</script>
+
+</body>
+</html>
+
+
